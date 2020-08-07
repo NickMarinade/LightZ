fefba9fe156e528b41a59e7c266abb2270a9d588
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-08-07 16:19:46 +0200

    I deleted my mailgun domain

--- src/Back/controllers/AuthController.js
+++ src/Back/controllers/AuthController.js
@@ -1,90 +1,88 @@
 const User = require("../models/User");
-const Report = require ("../models/Report")
+const Report = require("../models/Report");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const mailgun = require("mailgun-js");
 const DOMAIN = "sandboxfa74fb7d419d48faabce0bc40243fe39.mailgun.org";
-//const DOMAIN = "sandbox0e9d49063b8f4bc4ad0e48a040cad4b5.mailgun.org";
 const mg = mailgun({ apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN });
 const _ = require("lodash");
 
-
 const register1 = async (req, res) => {
   try {
-  let {userName, email, password, passwordCheck} = req.body;
+    let { userName, email, password, passwordCheck } = req.body;
 
-  //validation
+    //validation
 
-  if (!email || !password || !passwordCheck) {
-    return res.status(400).json({msg: 'Required field are empty'});
-  } 
-  else if (password.length < 5) {
-    return res.status(400).json({ msg: "Password must have at least 6 characters" });
-  } 
-  else if (password !== passwordCheck) {
-    return res.status(400).json({ msg: "Wrong password, enter the same password twice" });
-  } 
-  else if (!userName) {
-    userName = email;
+    if (!email || !password || !passwordCheck) {
+      return res.status(400).json({ msg: "Required field are empty" });
+    } else if (password.length < 5) {
+      return res
+        .status(400)
+        .json({ msg: "Password must have at least 6 characters" });
+    } else if (password !== passwordCheck) {
+      return res
+        .status(400)
+        .json({ msg: "Wrong password, enter the same password twice" });
+    } else if (!userName) {
+      userName = email;
+    }
+
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ msg: "An account with this email already exist" });
+    }
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      email,
+      password: passwordHash,
+      userName,
+    });
+    const savedUser = await newUser.save();
+    res.json(savedUser, { msg: "User added successfuly" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-
-  const existingUser = await User.findOne({email: email})
-  if (existingUser) {
-    return res.status(400).json({ msg: "An account with this email already exist" });
-  } 
-
-  const salt = await bcrypt.genSalt()
-  const passwordHash = await bcrypt.hash(password, salt)
-
-  const newUser = new User({
-    email,
-    password: passwordHash,
-    userName
-  });
-  const savedUser = await newUser.save();
-  res.json(savedUser, { msg: "User added successfuly" });
-
-}
-catch(err) {
-  res.status(500).json({error: err.message});
-}
-
 };
 
 const login1 = async (req, res) => {
   try {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     //validation
 
-    if(!email || !password) {
+    if (!email || !password) {
       return res.status(400).json({ msg: "Required field are empty" });
     }
 
     const user = await User.findOne({ email: email });
     if (!user) {
-      return res.status(400).json({ msg: "No account with this email has been found" });
+      return res
+        .status(400)
+        .json({ msg: "No account with this email has been found" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ msg: "Wrong password" });
     }
-   
+
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     res.json({
       token,
       user: {
         id: user._id,
         userName: user.userName,
-      }
-    })
-  }
-  catch(err) {
+      },
+    });
+  } catch (err) {
     res.status(500).json({ error: err.message });
   }
-
-}
+};
 
 const tokenIsValid = async (req, res) => {
   try {
@@ -96,7 +94,7 @@ const tokenIsValid = async (req, res) => {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     if (!verified) {
       return res.json(false);
-    } 
+    }
 
     const user = await User.findById(verified.id);
     if (!user) {
@@ -104,11 +102,10 @@ const tokenIsValid = async (req, res) => {
     } else {
       return res.json(true);
     }
-
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-}
+};
 
 const getUser = async (req, res) => {
   const user = await User.findById(req.user);
@@ -116,46 +113,47 @@ const getUser = async (req, res) => {
     userName: user.userName,
     id: user._id,
   });
-}
+};
 
 const report1 = async (req, res) => {
   try {
-    const {answer, city, state} = req.body;
+    const { answer, city, state } = req.body;
     const date_time = new Date();
-    const formattedDate = `${date_time.toDateString()} ${date_time.toLocaleTimeString()}`
-    const stringDate = formattedDate.toString()
-    
-  //validation
- 
-  if (answer === undefined){
-    return res.status(400).json({msg: 'Please specify if you have electricity'});
-  }
- 
-  if (city === undefined) {
-      return res.status(400).json({msg: 'Please specify your city'});
-    } 
-   
-  const newReport = new Report({
+    const formattedDate = `${date_time.toDateString()} ${date_time.toLocaleTimeString()}`;
+    const stringDate = formattedDate.toString();
+
+    //validation
+
+    if (answer === undefined) {
+      return res
+        .status(400)
+        .json({ msg: "Please specify if you have electricity" });
+    }
+
+    if (city === undefined) {
+      return res.status(400).json({ msg: "Please specify your city" });
+    }
+
+    const newReport = new Report({
       User,
       answer,
       cityName: city,
       stateName: state,
-      date:stringDate,
+      date: stringDate,
     });
     const savedReport = await newReport.save();
     res.json(savedReport);
-
-}
-catch(err) {
-  res.status(500).json({error: err.message});
-}
-}
-
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 const getReportYes = async (req, res, next) => {
   try {
-    const reports = await Report.find(req.reports1).sort({ createdAt: -1 }).limit(30);
-    const yes = reports.filter(i => i.answer === true);
+    const reports = await Report.find(req.reports1)
+      .sort({ createdAt: -1 })
+      .limit(30);
+    const yes = reports.filter((i) => i.answer === true);
     const userEmail = [];
     const reportsFiltered = [];
     yes.forEach((register1) => {
@@ -166,21 +164,21 @@ const getReportYes = async (req, res, next) => {
         userEmail.push(username);
         reportsFiltered.push(register1);
       }
-          });
+    });
     res.send(reportsFiltered);
-    } catch (error) {
+  } catch (error) {
     res.json({
       message: error.message,
     });
   }
-}
-
-
+};
 
 const getReportNo = async (req, res) => {
   try {
-    const reports = await Report.find(req.reports1).sort({ createdAt: -1 }).limit(26);
-    const no = reports.filter(i => i.answer === false);
+    const reports = await Report.find(req.reports1)
+      .sort({ createdAt: -1 })
+      .limit(26);
+    const no = reports.filter((i) => i.answer === false);
     const userEmail = [];
     const reportsFiltered = [];
     no.forEach((register1) => {
@@ -191,14 +189,14 @@ const getReportNo = async (req, res) => {
         userEmail.push(username);
         reportsFiltered.push(register1);
       }
-          });
+    });
     res.send(no);
-    } catch (error) {
+  } catch (error) {
     res.json({
       message: error.message,
     });
   }
-}
+};
 
 const forgotPassword = (req, res) => {
   const { email } = req.body;
@@ -224,20 +222,20 @@ const forgotPassword = (req, res) => {
     return user.updateOne({ resetLink: token }, function (err, success) {
       if (err) {
         return res.status(400).json({ msg: "Reset link error" });
-      } 
-        mg.messages().send(data, function (err, body) {
-          console.log(body)  
-          if (err) {
-            return res.json({
-              msg: err.message,
-            });
-          }if(success)
-          {return res.json({
+      }
+      mg.messages().send(data, function (err, body) {
+        console.log(body);
+        if (err) {
+          return res.json({
+            msg: err.message,
+          });
+        }
+        if (success) {
+          return res.json({
             msg: "email has been sent, please reset your password",
-          });}
-        
+          });
+        }
       });
-      
     });
   });
 };
@@ -293,22 +291,20 @@ const resetPassword = (req, res) => {
 const getMessage = (req, res) => {
   const { email, message } = req.body;
   const data = {
-    from: 'Excited User <Ligthz@teamapp.com>',
-    to: 'hyf.team.lightz@gmail.com',
-    subject:`'Hello form ${email}'`,
-    text: 'Testing some Mailgun awesomness !',
+    from: "Excited User <Ligthz@teamapp.com>",
+    to: "hyf.team.lightz@gmail.com",
+    subject: `'Hello form ${email}'`,
+    text: "Testing some Mailgun awesomness !",
     html: `'<html><h3>Message from the user ${email} here:</h3><h4>${message}</h4> </html>'`,
-    
   };
-  
+
   mg.messages().send(data, function (error, body) {
     console.log(body);
   });
-      return res.json({
-        msg: "email has been sent",
-      });
+  return res.json({
+    msg: "email has been sent",
+  });
 };
-
 
 module.exports = {
   report1,
@@ -321,7 +317,5 @@ module.exports = {
   getReportNo,
   forgotPassword,
   resetPassword,
-  getMessage
+  getMessage,
 };
-
-
