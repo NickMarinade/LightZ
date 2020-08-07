82bb65ca47f4281bd9c6d7fb87486d0c93dd37f7
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-08-07 16:16:52 +0200

    I hid my domain from mailgun for security reasons in an .env file

--- src/Back/api/controllers/AuthController.js
+++ src/Back/api/controllers/AuthController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const _ = require("lodash");
 require("dotenv").config();
 const mailgun = require("mailgun-js");
-const DOMAIN = "sandbox0e9d49063b8f4bc4ad0e48a040cad4b5.mailgun.org";
+const DOMAIN = process.env.MAILGUN_DOMAIN;
 const mg = mailgun({ apiKey: process.env.MAILGUN_APIKEY, domain: DOMAIN });
 // const { use } = require("../routes/auth");
 
